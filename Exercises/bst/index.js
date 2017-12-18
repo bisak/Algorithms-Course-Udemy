9395e6247a2b9b948926b8dd13282e96db56f839
Biser Atanasov <biser.p.atanasov@gmail.com>
2017-12-18 11:50:43 +0200

    Added some binary search tree stuff

--- Exercises/bst/index.js
+++ Exercises/bst/index.js
@@ -11,6 +11,60 @@
 // class.  Contains should accept a 'data' argument
 // and return the Node in the tree with the same value.
 
-class Node {}
+class Node {
+  constructor (data) {
+    this.left = null
+    this.data = data
+    this.right = null
+  }
 
-module.exports = Node;
+  insert (dataToInsert, currentNode = this) {
+    if (currentNode.data > dataToInsert) {
+      if (currentNode.left == null) {
+        currentNode.left = new Node(dataToInsert)
+      } else {
+        return this.insert(dataToInsert, currentNode.left)
+      }
+    }
+
+    if (currentNode.data < dataToInsert) {
+      if (currentNode.data < dataToInsert) {
+        if (currentNode.right == null) {
+          currentNode.right = new Node(dataToInsert)
+        } else {
+          return this.insert(dataToInsert, currentNode.right)
+        }
+      }
+    }
+  }
+
+  contains (searchData, currentNode = this) {
+    if (!currentNode) {
+      return null
+    }
+
+    if (searchData < currentNode.data) {
+      return this.contains(searchData, currentNode.left)
+    }
+
+    if (searchData > currentNode.data) {
+      return this.contains(searchData, currentNode.right)
+    }
+
+    if (searchData === currentNode.data) {
+      return currentNode
+    }
+  }
+
+}
+
+const node = new Node(10)
+node.insert(5)
+node.insert(15)
+node.insert(17)
+node.insert(12)
+node.insert(13)
+
+console.log(node.contains(12))
+
+module.exports = Node
